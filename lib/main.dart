0405aa5f7c6f20f878980582ad98d98c6e8d8995
tck <tck@dra100.com>
2019-04-23 11:11:44 +0800

    appbar上面的icon

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.yellow),
-      home: Scaffold(
-
-        appBar: AppBar(
-          title: Text(
-            "Study_flutter",
-          ),
-          elevation: 0.0,
-        ),
-        body: ListViewStudy(),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.yellow),
+        home: Home());
   }
 }
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+          ),
+          onPressed: () => {debugPrint('Navigation button is pressed')},
+          tooltip: "Navigation",
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+            ),
+            onPressed: () => {debugPrint('search button is pressed')},
+            tooltip: "Search",
+          ),
+        ],
+        title: Text(
+          "Study_flutter",
+        ),
+        elevation: 0.0,
+      ),
+      body: null,
+    );
+  }
+}
