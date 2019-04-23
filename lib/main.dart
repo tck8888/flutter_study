48ae76ebf13d9092eca5a50f680500c3f876574a
tck <tck@dra100.com>
2019-04-23 11:24:53 +0800

    自定义按钮水波纹样式

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.yellow),
+        theme: ThemeData(primarySwatch: Colors.yellow,
+            highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+        splashColor: Colors.white70,),
         home: Home());
   }
 }
