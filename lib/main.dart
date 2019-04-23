fb37dd178b3459775fc7e10c2ffaa4366b9c5432
tck <tck@dra100.com>
2019-04-23 11:20:50 +0800

    tabbar的样式使用

--- lib/main.dart
+++ lib/main.dart
@@ -16,31 +16,48 @@ class MyApp extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-          ),
-          onPressed: () => {debugPrint('Navigation button is pressed')},
-          tooltip: "Navigation",
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.search,
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(
+                Icons.menu,
+              ),
+              onPressed: () => {debugPrint('Navigation button is pressed')},
+              tooltip: "Navigation",
             ),
-            onPressed: () => {debugPrint('search button is pressed')},
-            tooltip: "Search",
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.search,
+                ),
+                onPressed: () => {debugPrint('search button is pressed')},
+                tooltip: "Search",
+              ),
+            ],
+            title: Text(
+              "Study_flutter",
+            ),
+            elevation: 0.0,
+            bottom: TabBar(
+                unselectedLabelColor: Colors.black38,
+                indicatorColor: Colors.black54,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorWeight: 1.0,
+                tabs:[
+              Tab(icon: Icon(Icons.local_florist),),
+              Tab(icon: Icon(Icons.change_history),),
+              Tab(icon: Icon(Icons.directions_bike),)
+            ]),
           ),
-        ],
-        title: Text(
-          "Study_flutter",
-        ),
-        elevation: 0.0,
-      ),
-      body: null,
+          body: TabBarView(children: [
+            Icon(Icons.local_florist,size: 128.0, color: Colors.black12,),
+            Icon(Icons.change_history,size: 128.0, color: Colors.black12,),
+            Icon(Icons.directions_bike,size: 128.0, color: Colors.black12,),
+          ]),
+        )
     );
   }
 }
